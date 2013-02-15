f2253445f8a9a41da11b52b88a2a5a976647ad6d
mcartaud <mathieu.cartaud@obeo.fr>
2013-02-15 13:56:03 +0100

    change JVM from 1.7 to 1.5

--- plugins/org.obeonetwork.dsl.gen.eclipse.model/src-gen/org/obeonetwork/dsl/gen/eclipse/View.java
+++ plugins/org.obeonetwork.dsl.gen.eclipse.model/src-gen/org/obeonetwork/dsl/gen/eclipse/View.java
@@ -3,7 +3,6 @@
 package org.obeonetwork.dsl.gen.eclipse;
 
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.ecore.EObject;
 
 /**
  * <!-- begin-user-doc -->
--- plugins/org.obeonetwork.dsl.gen.eclipse.model/src-gen/org/obeonetwork/dsl/gen/eclipse/impl/ViewImpl.java
+++ plugins/org.obeonetwork.dsl.gen.eclipse.model/src-gen/org/obeonetwork/dsl/gen/eclipse/impl/ViewImpl.java
@@ -3,20 +3,17 @@
 package org.obeonetwork.dsl.gen.eclipse.impl;
 
 import java.util.Collection;
+
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
-
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
-import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
-
 import org.eclipse.emf.ecore.util.EObjectContainmentEList;
 import org.eclipse.emf.ecore.util.EObjectWithInverseResolvingEList;
 import org.eclipse.emf.ecore.util.InternalEList;
 import org.obeonetwork.dsl.gen.eclipse.Action;
-import org.obeonetwork.dsl.gen.eclipse.DynamicHelp;
 import org.obeonetwork.dsl.gen.eclipse.EclipsePackage;
 import org.obeonetwork.dsl.gen.eclipse.Menu;
 import org.obeonetwork.dsl.gen.eclipse.Perspective;
